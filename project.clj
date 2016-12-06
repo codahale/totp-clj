f3194b51d5f4e5503cb7626c50e39e77613ee785
Coda Hale <coda.hale@gmail.com>
2016-12-06 15:42:43 -0800

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.codahale/totp-clj "0.1.0-SNAPSHOT"
+(defproject com.codahale/totp-clj "0.1.0"
   :description "A library for implementing TOTP."
   :url "https://github.com/codahale/totp-clj"
   :license {:name "Eclipse Public License"
